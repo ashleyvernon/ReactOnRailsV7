9ddcf6931f297aebe7c2160e82d0c0281e1a2520
ashleyvernon <avernon526@gmail.com>
2017-10-04 14:38:43 -0700

    updates procfile, sets compile to false since not using webpacker

--- config/initializers/react_on_rails.rb
+++ config/initializers/react_on_rails.rb
@@ -3,6 +3,7 @@
 # See docs/basics/configuration.md for many more options
 
 ReactOnRails.configure do |config|
+  config.node_modules_location = "client"
   # This configures the script to run to build the production assets by webpack. Set this to nil
   # if you don't want react_on_rails building this file for you.
   config.build_production_command = "RAILS_ENV=production bin/webpack"
